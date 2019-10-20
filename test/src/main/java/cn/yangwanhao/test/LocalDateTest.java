6a4f04fdcb8d5914e31481f85584fb568342a13f
yangwanhao <yangwanhao163@163.com>
2019-10-20 20:54:16 +0800

    新建test模块

---
+++ test/src/main/java/cn/yangwanhao/test/LocalDateTest.java
@@ -0,0 +1,29 @@
+package cn.yangwanhao.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * LocalDateTest
+ *
+ * @author 杨万浩
+ * @version 1.0.0
+ * @date 2019/10/20 19:46
+ */
+public class LocalDateTest {
+
+    public static void main(String[] args) {
+        // LocalDate
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        // LocalTime
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+        // LocalDateTime
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+        System.out.println(localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+}
