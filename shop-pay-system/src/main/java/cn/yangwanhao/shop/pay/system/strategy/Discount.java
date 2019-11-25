29319a992f09943c2f009090e5e7322314b37d17
yangwanhao <yangwanhao163@163.com>
2019-11-25 23:04:18 +0800

    新增策略模式写的超市打折系统

---
+++ shop-pay-system/src/main/java/cn/yangwanhao/shop/pay/system/strategy/Discount.java
@@ -0,0 +1,23 @@
+package cn.yangwanhao.shop.pay.system.strategy;
+
+import java.math.BigDecimal;
+
+/**
+ * Strategy
+ *
+ * @author 杨万浩
+ * @version 1.0.0
+ * @date 2019/10/21 22:08
+ */
+public interface Discount {
+
+    /**
+     * description
+     *
+     * @param num 总金额
+     * @author 杨万浩
+     * @return 打折后的金额
+     * @date 2019/10/21 22:09
+     */
+    BigDecimal discount(BigDecimal num);
+}
