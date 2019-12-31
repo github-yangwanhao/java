29e220835521e0db6822dd34f65494f13552f8ae
yangwanhao <yangwanhao163@163.com>
2019-12-31 17:53:47 +0800

    Git Test

---
+++ test/src/main/java/cn/yangwanhao/test/GitTest.java
@@ -0,0 +1,17 @@
+package cn.yangwanhao.test;
+
+/**
+ * description
+ *
+ * @author 杨万浩
+ * @version 1.0.0
+ * @date 2019/12/31 17:53
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
