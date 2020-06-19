dcafa99eb10de6267ea21d4127ce0193ddd3edf7
杨万浩 <yangwanhao163@163.com>
2020-06-19 13:03:46 +0800

    基本类型默认值测试

--- test/src/main/java/cn/yangwanhao/test/BasicTypeTest.java
+++ test/src/main/java/cn/yangwanhao/test/BasicTypeTest.java
@@ -1,6 +1,8 @@
 package cn.yangwanhao.test;
 
-import cn.yangwanhao.test.bean.BasicType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * description
@@ -14,7 +16,7 @@ public class BasicTypeTest {
 
     public static void main(String[] args) {
         /**
-         * 有结果可见,char类型的默认值是 \u0000 表现形式是一个空格 但是不等于一个空格
+         * 由结果可见,char类型的默认值是 \u0000 表现形式是一个空格 但是不等于一个空格
          */
         BasicType basicType = new BasicType();
         // BasicType(byteValue=0, shortValue=0, intValue=0, longValue=0, doubleValue=0.0, floatValue=0.0, charValue= , booleanValue=false, string=null, basicType=null)
@@ -26,11 +28,36 @@ public class BasicTypeTest {
         System.out.println(' ' == c);
         // false
         System.out.println(' ' == basicType.getCharValue());
-        int i = (int) c;
         // \u0000 转化为整数后是0
-        System.out.println(i);
+        System.out.println((int) c);
         // true
         System.out.println(basicType.getCharValue() == c);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class BasicType {
+        // byte 0
+        private byte byteValue;
+        // short 0
+        private short shortValue;
+        // int 0
+        private int intValue;
+        // long 0
+        private long longValue;
+        // double 0.0
+        private double doubleValue;
+        // float 0.0
+        private float floatValue;
+        // char \u0000 这个字符代表空
+        private char charValue;
+        // boolean false
+        private boolean booleanValue;
+        // null
+        private String string;
+        // null
+        private BasicType basicType;
     }
 
 }
