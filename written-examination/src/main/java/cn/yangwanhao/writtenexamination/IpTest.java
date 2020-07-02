0d773747a397c8ce09bd5b5ef1455eeccff041df
杨万浩 <yangwanhao163@163.com>
2020-07-02 10:50:21 +0800

    修改命名和注释

--- written-examination/src/main/java/cn/yangwanhao/writtenexamination/IpTest.java
+++ written-examination/src/main/java/cn/yangwanhao/writtenexamination/IpTest.java
@@ -5,28 +5,36 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 /**
- * description IPV4Test类
+ * description IpTest类
  *
  * @author 杨万浩
  * @date 2020/6/6 16:36
  */
-public class IPV4Test {
+public class IpTest {
+
+    private static final Integer IPV4_LENGTH = 4;
 
     public static void main(String[] args) {
+        /*
+         * 题目：输入一个字符串,判断是否为IPV4地址
+         */
         Scanner in = new Scanner(System.in);
         System.out.println("请输入一个IPV4地址");
         String ipStr = in.nextLine();
-        System.out.println(isIPV4(ipStr));
+        System.out.println(isIpAddress(ipStr));
     }
 
-    private static Boolean isIPV4(String ipStr) {
+    private static Boolean isIpAddress(String ipStr) {
+        // 为空则false
         if (StringUtils.isBlank(ipStr)) {
             return false;
         }
         String[] array = ipStr.split("\\.");
-        if (array.length != 4) {
+        // 分割后字符串数组长度不等于4则false
+        if (array.length != IPV4_LENGTH) {
             return false;
         }
+        // 判断每个数字的范围 0 <= num <= 255
         for (String str : array) {
             int num = Integer.parseInt(str);
             if (num < 0 || num > 255) {
