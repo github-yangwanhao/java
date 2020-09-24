37befa3942ee380a11e6b10555b90a53c2da3b9d
杨万浩 <yangwanhao163@163.com>
2020-09-24 15:05:20 +0800

    获取当前时间到明天凌晨的秒数

--- test/src/main/java/cn/yangwanhao/test/DateTest.java
+++ test/src/main/java/cn/yangwanhao/test/DateTest.java
@@ -2,6 +2,7 @@ package cn.yangwanhao.test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -13,7 +14,7 @@ import java.util.Date;
  */
 
 public class DateTest {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, InterruptedException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date date1 = sdf.parse("2019-11-12");
         Date date2 = sdf.parse("2019-11-13");
@@ -25,6 +26,10 @@ public class DateTest {
         System.out.println(date1.getMonth() == date3.getMonth());
         System.err.println(differentDaysByMillisecond(date1, date2));
         System.err.println(differentDaysByMillisecond1(date1, date2));
+        while (true) {
+            System.out.println(getSecondsNextEarlyMorning());
+            Thread.sleep(1000);
+        }
     }
 
     private static int differentDaysByMillisecond(Date date1,Date date2) {
@@ -35,5 +40,15 @@ public class DateTest {
     private static long differentDaysByMillisecond1(Date date1,Date date2) {
         long days = (date2.getTime() - date1.getTime()) / (1000*3600*24);
         return Math.abs(days);
+    }
+
+    public static Long getSecondsNextEarlyMorning() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_YEAR, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return (cal.getTimeInMillis() - System.currentTimeMillis()) / 1000;
     }
 }
