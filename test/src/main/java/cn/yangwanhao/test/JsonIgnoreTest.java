d5fef6db5a2e4086f891f1f8299c3d3b727ff5d1
杨万浩 <yangwanhao163@163.com>
2021-03-04 14:49:10 +0800

    测试转JSON时忽略某些字段

--- test/src/main/java/cn/yangwanhao/test/JsonIgnoreTest.java
+++ test/src/main/java/cn/yangwanhao/test/JsonIgnoreTest.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,9 +26,9 @@ public class JsonIgnoreTest {
         bean.setBbb(1);
         bean.setCcc(new byte[] {'1', '2', '3', '4', '5', '6'});
         bean.setDdd('a');
-        System.out.println(JSONObject.toJSONString(bean.getCcc()));
-        System.out.println(JSONObject.toJSONString(bean.getDdd()));
         System.out.println(JSONObject.toJSONString(bean));
+        String beanIgnore = JsonIgnoreUtil.toJsonStringIgnoreField(bean, "bbb");
+        System.out.println(beanIgnore);
     }
 
 }
@@ -44,4 +46,14 @@ class Bean implements Serializable {
     private byte[] ccc;
 
     private transient char ddd;
+}
+
+class JsonIgnoreUtil {
+
+    public static String toJsonStringIgnoreField(Object object, String... fields) {
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(fields);
+        return JSONObject.toJSONString(object, excludefilter, SerializerFeature.WriteNullStringAsEmpty);
+    }
 }
